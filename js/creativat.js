8859c7f5893ab93321bcb2787b0d54222aa65ca9
nita86 <ana.rodriguezalv@gamil.com>
2019-10-11 14:10:48 +0200

    menú modificado. Falta poner imagenes

--- js/creativat.js
+++ js/creativat.js
@@ -1,5 +1,19 @@
 //funciones Javascript CreativaT
 
+//---------slide_down-----------
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("slide_down").style.top = "0";
+  } else {
+    document.getElementById("slide_down").style.top = "-100px";
+  }
+}
+
+//--------lightbox----------------
 function abrir() {
     document.getElementById("cuadroGrande").style.display="block";
 }
@@ -9,7 +23,6 @@ function cerrar() {
 function cerrar2() {
     document.getElementById("cuadroGrande").style.display="none";
 }
-//--------lightbox----------------
 
 function fotosSrc(src) {//sacamos la información de this.src y la traemos a través del parametro src. 
     var foto='<img src="'+src+'">';
