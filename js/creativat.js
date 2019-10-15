2758aed92632fe24f088ec86798c60657e81388b
nita86 <ana.rodriguezalv@gamil.com>
2019-10-15 11:58:11 +0200

    15/10

--- js/creativat.js
+++ js/creativat.js
@@ -1,6 +1,6 @@
 //funciones Javascript CreativaT
 
-//---------slide_down-----------
+//---------ENCABEZADO slide_down-----------
 // When the user scrolls down 20px from the top of the document, slide down the navbar
 // When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
 window.onscroll = function() {scrollFunction()};
@@ -13,7 +13,34 @@ function scrollFunction() {
   }
 }
 
-//--------lightbox----------------
+//------------TALLERES 19/20 slideshow--------------------
+var slideIndex = 1;
+showSlides(slideIndex);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+//--------TALLERES 18/19 lightbox----------------
 function abrir() {
     document.getElementById("cuadroGrande").style.display="block";
 }
